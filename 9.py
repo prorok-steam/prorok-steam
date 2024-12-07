a6805d44da0f00c8e6f672b7575b6a3460bb44b5
prorok-steam <2006solovjovlad@gmail.com>
2024-12-07 13:20:11 +0300

    Add files via upload

--- 9.py
+++ 9.py
@@ -18,7 +18,7 @@ plt.xlabel('Абсцисс')
 plt.ylabel('Ординат')
 plt.legend()
 plt.show()
-# plt.savefig('/my_first_dependence.svg')
+plt.savefig(dir + '9_1.png', dpi = 300)
 
 #2
 plt.plot(x, f(x, 1, 1), label = '1', color = 'indianred')
@@ -47,71 +47,72 @@ plt.savefig(dir + '9_2.png', dpi = 300)
 
 #3
 dir = os.getcwd()
-x = np.linspace(-5,0,1000)
-plt.rcParams['figure.figsize'] = [5,5]
+x = np.linspace(-5, 0, 1000)
+plt.rcParams['figure.figsize'] = [5, 5]
 plt.rcParams['figure.autolayout'] = True
 
-def f(x,a,b):
-    y = (x**b+a**b)/x**b
-    y[y > 20] = np.nan
-    y[y < -20] = np.nan
+def f(x, alpha, beta):
+    y = (x ** beta + alpha ** beta) / x ** beta
+    y[y > 100] = np.nan
+    y[y < -100] = np.nan
     return y
 
-plt.plot(x,f(x,1,1), color = 'red', label = '1 случай')
-plt.plot(x,f(x,2,1), color = 'green', label = '2 случай')
-plt.plot(x,f(x,1,2), color = 'blue', label = '3 случай')
-plt.plot(x,[0]*len(x), color = 'purple', label = 'f(x)=0')
+plt.plot(x,f(x, 1, 1), color = 'indianred', label = '1 случай')
+plt.plot(x,f(x, 2, 1), color = 'palegoldenrod', label = '2 случай')
+plt.plot(x,f(x, 1, 2), color = 'darkblue', label = '3 случай')
+plt.plot(x, [0] * len(x), color = 'gold', label = 'f(x) = 0')
 plt.grid()
-plt.title('Заголовок')
-plt.xlabel('ось абсцисс')
-plt.ylabel('ось ординат')
+plt.title('График')
+plt.xlabel('Абсцисс')
+plt.ylabel('Ординат')
 plt.legend()
 plt.axes([0.21, 0.17, 0.50, 0.25])
 
-def f(x,a,b):
-    y = (x**b+a**b)/x**b
+def f(x, alpha, beta):
+    y = (x ** beta + alpha ** beta) / x ** beta
     y[y > 3] = np.nan
     y[y < -3] = np.nan
     return y
 
-x = np.linspace(-5,0,1000)
-plt.plot(x,[0]*len(x), color = 'purple', label = 'x=0')
+x = np.linspace(-5, 0, 1000)
+plt.plot(x, [0] * len(x), color = 'purple', label = 'x = 0')
 
 #4
 dir = os.getcwd()
-x = np.linspace(-5,5,1000)
-plt.rcParams['figure.figsize'] = [5,5]
+x = np.linspace(-5, 5, 1000)
+plt.rcParams['figure.figsize'] = [3, 3]
 plt.rcParams['figure.autolayout'] = True
 
-def f(x,a,b):
-    y = (x**b+a**b)/x**b
-    y[y > 20] = np.nan
-    y[y < -20] = np.nan
+def f(x, alpha, beta):
+    y = (x ** beta + alpha ** beta) / x ** beta
+    y[y > 100] = np.nan
+    y[y < -100] = np.nan
     return y
 
-plt.plot(x,f(x,1,0.5), color = 'red', label = '1 случай')
-plt.plot(x,f(x,1,-0.5), color = 'green', label = '2 случай')
-plt.plot(x,f(x,1,-1.5), color = 'blue', label = '3 случай')
-plt.plot(x,[0]*len(x), color = 'purple', label = 'f(x)=0')
+plt.plot(x,f(x, 1, 1), color = 'indianred', label = '1 случай')
+plt.plot(x,f(x, 2, 1), color = 'palegoldenrod', label = '2 случай')
+plt.plot(x,f(x, 1, 2), color = 'darkblue', label = '3 случай')
+plt.plot(x, [0] * len(x), color = 'gold', label = 'f(x) = 0')
 plt.grid()
-plt.title('Заголовок')
-plt.xlabel('ось абсцисс')
-plt.ylabel('ось ординат')
+plt.title('График')
+plt.xlabel('Абсцисс')
+plt.ylabel('Ординат')
 plt.legend()
-x = np.linspace(-1,2,1000)
-plt.savefig(dir+'/hometask9(2).png', dpi=300)
+x = np.linspace(-1, 2, 1000)
 
-def create_plot(ax,x,a,b1,b2,array_b,k,fontsize=12,hide_labels=False):
-    ax.plot(x,f(x,a,b1), color = 'red', label = '1 случай')
-    ax.plot(x,f(x,a,b2), color = 'green', label = '2 случай')   
+def C_P(ax, x, a, b1, b2, array_b, k, fontsize = 12, hide_labels = False):
+    ax.plot(x, f(x, a, b1), color = 'orange', label = '1 случай')
+    ax.plot(x, f(x, a, b2), color = 'yellow', label = '2 случай')   
     ax.grid()
-    ax.plot(x,f(x,a,array_b[k][0]), color = 'blue', label = '1 случай')
-    ax.plot(x,f(x,a,array_b[k][1]), color = 'pink', label = '2 случай')
+    ax.plot(x, f(x, a, array_b[k][0]), color = 'darkblue', label = '1 случай')
+    ax.plot(x, f(x, a, array_b[k][1]), color = 'pink', label = '2 случай')
     ax.legend()
-    ax.locator_params(nbins=3)
+    ax.locator_params(nbins = 3)
     if hide_labels:
         ax.set_xticklabels([])   
         ax.set_yticklabels([]) 
     else:
-        ax.set_xticklabels('ось абсцисс',fontsize=fontsize)   
-        ax.set_yticklabels('ось ординат',fontsize=fontsize)
+        ax.set_xticklabels('Абсицсс', fontsize = fontsize)
+        ax.set_yticklabels('Ординат', fontsize = fontsize)
+
+plt.savefig(dir + '/9_4.png', dpi = 300)
